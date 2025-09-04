54e10e7aa7201dd1796caf09d67ebc9da965356a
nawresssjebali <115501221+nawresssjebali@users.noreply.github.com>
2025-09-04 15:33:04 +0100

    Update main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,11 +1,7 @@
-// src/main.server.ts
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { platformServer, renderModule } from '@angular/platform-server';
-import { AppServerModule } from './app/app.server.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-if (environment.production) {
-  enableProdMode();
-}
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-export { AppServerModule };
+export default bootstrap;
